9c8cae889f2d302e7f6d86ef50a8d8a67db05aa7
Adbaa006 <adbaa007@hotmail.com>
2026-04-30 11:21:21 +0200

    Første forsøk på innlogging

--- app.py
+++ app.py
@@ -22,9 +22,60 @@ def main():
 def kontakt():
     return render_template('kontakt.html')
 
-@app.route("/innlogging")
+@app.route("/innlogging", methods = ['GET', 'POST'])
 def innlogging():
-    return render_template('innlogging.html')
+    message = ''
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE email = % s AND password = % s', (email, password,))
+        user = cursor.fetchone()
+        if user:
+            session['loggedin'] = True
+            session['userid'] = user['userid']
+            session['name'] = user['name']
+            session['email'] = user['email']
+            message = 'Innlogging vellykket!'
+            return render_template('konto.html', message = message)
+        else:
+            message = 'Vennligst skriv inn riktig E-postadresse og passord'
+            return render_template('innlogging.html')
+
+@app.route("/loggut")
+def loggut():
+    session.pop('loggedin', None)
+    session.pop('userid', None)
+    session.pop('email', None)
+    return redirect(url_for('innlogging'))
+
+@app.route("/registrere",methods = ['GET', 'POST'])
+def registrere():
+    message = ''
+    if request.method == 'POST' and 'name' in request.form and 'password' in request.form and 'email' in request.form:
+        userName = request.form['name']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE email = % s', (email))
+        account == cursor.fetchone()
+        if account:
+            message = 'Brukeren er allerede opprettet'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            message = 'E-post er ikke gyldig'
+        elif not userName or not password or not email:
+            message = 'Vennligst fyll inn all informasjon'
+        else:
+            cursor.execute('INSERT INTO user VALUES (NULL, % s, % s, % s)', (userName, email, password))
+            mysql.connection.commit()
+            message = 'Bruker opprettet'
+    elif request.method == 'POST':
+        message = 'Vennligst fyll inn all informasjon'
+    return render_template("/registrere", message = message)
+
+@app.route("/konto")
+def konto():
+    return render_template('konto.html')
 
 @app.route("/produkter/<type>")
 def produkter(type):
